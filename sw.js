e20a7eef3f7d59bd1bab161677e01e142f3c50de
ramuse112-design <ramuse112@gmail.com>
2026-07-17 17:56:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
+self.addEventListener('install', (event) => {
+  // Fuerza al Service Worker a activarse inmediatamente
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  // Toma el control de la app al momento
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
-  // Código mínimo para activar la instalación completa en Chrome
+  // Responde con la red de forma normal (requisito obligatorio de Chrome)
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
